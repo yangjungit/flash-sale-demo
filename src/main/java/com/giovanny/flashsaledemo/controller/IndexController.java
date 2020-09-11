034417d622fd3c51f69442923806e117896c795c
yangjun <1102934697@qq.com>
2020-09-11 17:58:07 +0800

    test

--- src/main/java/com/giovanny/flashsaledemo/controller/IndexController.java
+++ src/main/java/com/giovanny/flashsaledemo/controller/IndexController.java
@@ -18,4 +18,23 @@ public class IndexController {
     public ModelAndView index() {
         return new ModelAndView("index");
     }
+
+
+    public static void main(String[] args) {
+        String regex = "^.{2,20}$";
+        String name = "2s";
+        boolean matches = name.matches(regex);
+        System.out.println(matches);
+
+        String idNumRegex = "^[1-9]\\d{5}(18|19|([23]\\d))\\d{2}((0[1-9])|(10|11|12))(([0-2][1-9])|10|20|30|31)\\d{3}[0-9Xx]$";
+        String idNum = "513128199508082138";
+        boolean matches1 = idNum.matches(idNumRegex);
+        System.out.println(matches1);
+
+        String phoneRegex = "^1[2-9]\\d{9}$";
+        String phone = "10582457312";
+        boolean matches2 = phone.matches(phoneRegex);
+        System.out.println(matches2);
+
+    }
 }
